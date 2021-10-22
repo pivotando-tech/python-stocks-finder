f2e0bc73d3ce8ebf35996ca4a9ef85148a55bcdd
Filipe Marques <filipi.marques@gmail.com>
2021-10-22 08:40:07 -0300

    create wallet parameters

---
+++ entities/wallet.py
@@ -0,0 +1,10 @@
+from entities.user import User
+
+
+class Wallet(object):
+    ownerName = ''
+    stocks = []
+
+    def __init__(self, ownerName, stocks):
+        self.ownerName = ownerName
+        self.stocks = stocks
