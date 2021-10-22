d94250f6fabb6cfb64ecef76bace1c8dfeb5782a
Guto <gutembergdomingos13@gmail.com>
2021-10-21 22:43:27 -0300

    fix: uuid now is different when call function more one time

--- entities/user.py
+++ entities/user.py
@@ -2,11 +2,12 @@ import uuid
 
 
 class User(object):
-    id = uuid.uuid4()
+    id = ''
     name = ''
     wallet = []
 
     def __init__(self, name, wallet):
+        self.id = uuid.uuid4()
         self.name = name
         self.wallet = wallet
 
