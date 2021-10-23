95d2b433bd3dc9ca0fee18614d1ba054d5cb2b51
Guto <gutembergdomingos13@gmail.com>
2021-10-23 14:57:43 -0300

    retirado arquivo main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from entities.user import makeUser
-
-newUser = makeUser('Berg', [1234])
-segUser = makeUser('Filipe', [4321])
-print(newUser.id)
-print(segUser.id)
--- stock.py
+++ stock.py
@@ -2,7 +2,7 @@
 class stock:
     def __init__(self, name, description, price, update_at):
         self.name = name
-        self.description = description
+        self.description = descripgit tion
         self.price = price
         self.update_at = update_at
 
