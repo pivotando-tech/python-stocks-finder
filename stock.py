c72274bd46dad0f7dad83819532507a24183052e
Guto <gutembergdomingos13@gmail.com>
2021-10-23 15:19:50 -0300

    corrigido erro de digitação

--- stock.py
+++ stock.py
@@ -2,7 +2,7 @@
 class stock:
     def __init__(self, name, description, price, update_at):
         self.name = name
-        self.description = descripgit tion
+        self.description = description
         self.price = price
         self.update_at = update_at
 
