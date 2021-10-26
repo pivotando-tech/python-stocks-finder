23ca99184677870a8a000511f4585cfab0e52543
Filipe Marques <filipi.marques@gmail.com>
2021-10-25 21:54:26 -0300

    finalize all tasks

--- inputs/user.py
+++ inputs/user.py
@@ -1,12 +1,15 @@
-from masagens import userMasages
+from messages import userMasages
+
 
 
 def actionChoice():
     print(userMasages.viewWalletMsg, "digite [1]")
     print(userMasages.researchStocksMsg, "digite [2]")
+# TODO: Fazer uma verificação para só permitir o user digitar 1 ou 2 [Fazer verificação numa funcao externa]
     option = int(input('Opção: '))
     if option == 1:
         print("levar para a tela de visualizar carteira")
     else:
         print("Levar para a tela de pesquisa de ações")
+
 
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from masagens.criarArrayUsuario import userCarteiraInvest
+from messages.criarArrayUsuario import userCarteiraInvest
+from messages.welcome import welcome
 from inputs.user import actionChoice
+
+
+welcome()
 
 if len(userCarteiraInvest) == 0:
     print('Não existe usuário')
