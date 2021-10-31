0f48734668892cbfc5d6e50486e2bfc70cb7bc3f
Filipe Marques <filipi.marques@gmail.com>
2021-10-30 23:49:14 -0300

    feat: completed wallet class with its own properties

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from messages.criarArrayUsuario import userCarteiraInvest
 from messages.welcome import welcome
 from inputs.user import actionChoice
 
-
 welcome()
 
 if len(userCarteiraInvest) == 0:
     print('Não existe usuário')
 else:
     actionChoice()
-
-
---
+++ service/api.py
@@ -0,0 +1,18 @@
+import requests
+from requests.exceptions import HTTPError
+
+from service import strings
+
+
+def requestStock(symbol: str):
+    try:
+        getStockInfo = requests.get(strings.baseUrl, params={'symbol': symbol})
+        getStockInfo.raise_for_status()
+        response = getStockInfo.json()
+
+        return response
+
+    except HTTPError as http_err:
+        print(f'Http error: {http_err}')
+    except Exception as err:
+        print(f'Unexpected error: {err}')
