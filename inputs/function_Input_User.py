07621a841a875a55b889d05841b323fa4f1b825b
Guto <gutembergdomingos13@gmail.com>
2021-10-31 23:34:13 -0300

    resolvido TODO de opção de usuário

--- inputs/function_Input_User.py
+++ inputs/function_Input_User.py
@@ -1,14 +1,8 @@
-from messages import userMasages
 from views.WalletScreen import walletScreen
 from messages.erroOption import invalidOption
-from inputs.function_Input_User import opcaoUser
 
-def actionChoice():
-    print(userMasages.viewWalletMsg, "digite [1]")
-    print(userMasages.researchStocksMsg, "digite [2]")
-    # TODO: Fazer uma verificação para só permitir o user digitar 1 ou 2 [Fazer verificação numa função externa]
-    option = 0
 
+def opcaoUser():
     while option != 1 and option != 2:
         option = int(input('Opção: '))
 
@@ -18,3 +12,7 @@ def actionChoice():
             print("Levar para a tela de pesquisa de ações")
         else:
             invalidOption()
+
+
+
+
---
+++ messages/erroOption.py
@@ -0,0 +1,8 @@
+optionInvalidMsg = 'Opção inválida' \
+                   ' as opções são: [1] para visualizar sua carteira' \
+                   ' [2] para pesquisar novas ações:'
+
+
+
+def invalidOption():
+    print(optionInvalidMsg)
