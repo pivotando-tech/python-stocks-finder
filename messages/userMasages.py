27db8726563ce02c186c400cfb1113912c84f4a6
Guto <gutembergdomingos13@gmail.com>
2021-11-14 19:25:41 -0300

    merge with dev

--- messages/userMasages.py
+++ messages/userMasages.py
@@ -1,5 +1,12 @@
 viewWalletMsg = 'Você quer visualizar sua carteira?'
 researchStocksMsg = 'Você gostaria de pesquisar novas ações?'
+wantsToRegisterNewUserMsg = '''
+Você não está cadastrado, para salvar as ações em uma carteira, você precisa se cadastrar conosco.
+É bem simples, vamos nessa?
+'''
+forRegisterMsg = 'Para se registrar, digite'
+forSkipRegistrationMsg = 'Para pular essa etapa, digite'
+invalidChoiceMsg = 'Opção inválida, digite [1] ou [2]'
 sistemOptionMsg = '''[1] Para Visualizar carteira. 
 [2] Para pesquisar novas ações.
 [3] Para finalizar programa'''
