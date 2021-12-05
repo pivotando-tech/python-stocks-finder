47d6081aa6a47eaf907d2b44fd2f23151bea2d26
Guto <gutembergdomingos13@gmail.com>
2021-12-05 20:42:14 -0300

    wip: ensure user can see your wallet after add stocks

--- inputs/selectScreens.py
+++ inputs/selectScreens.py
@@ -1,4 +1,5 @@
 from utils.userFileHandler import readFile
+from views.allUserOptions import showAllOptions
 from views.showStocksInfo import stockInfoAndOptions
 from messages.userMasages import systemOptionMsg, systemOptionMsgWithOutStock
 from views.WalletScreen import walletScreen
@@ -13,28 +14,13 @@ def actionChoice(userName: str = ''):
     option = ''
 
     infoWallet = readFile().get('wallet')
+
+
     if len(infoWallet) != 0:
 
-
-        while option != '3':
-            print(systemOptionMsg)
-            option = str(input('Qual sua opção: '))
-            sleep(1)
-
-            if option == '1':
-                walletScreen()
-                sleep(5)
-            elif option == '2':
-                stockInfoAndOptions()
-                sleep(3)
-            elif option == '3':
-                print('Finalizando...')
-                sleep(1)
-            else:
-                invalidOption()
-                sleep(1)
-
+        showAllOptions(option)
     else:
+
         while option != '2':
             print(systemOptionMsgWithOutStock)
             option = str(input('Qual sua opção: '))
@@ -43,6 +29,11 @@ def actionChoice(userName: str = ''):
             if option == '1':
                 stockInfoAndOptions()
                 sleep(3)
+                updateWallet = readFile().get('wallet')
+
+                if len(updateWallet) != 0:
+                    showAllOptions(option)
+
             elif option == '2':
                 print('Finalizando...')
                 sleep(1)
---
+++ views/allUserOptions.py
@@ -0,0 +1,28 @@
+from messages.userMasages import systemOptionMsg
+from messages.valorinvalido import invalidOption
+from utils.userFileHandler import readFile
+from views.WalletScreen import walletScreen
+from views.showStocksInfo import stockInfoAndOptions
+from time import sleep
+
+
+def showAllOptions(option):
+    while option != '3':
+        print(systemOptionMsg)
+        option = str(input('Qual sua opção: '))
+        sleep(1)
+
+        if option == '1':
+            updateWallet = readFile().get('wallet')
+            if len(updateWallet) != 0:
+                walletScreen()
+                sleep(5)
+        elif option == '2':
+            stockInfoAndOptions()
+            sleep(3)
+        elif option == '3':
+            print('Finalizando...')
+            sleep(1)
+        else:
+            invalidOption()
+            sleep(1)
